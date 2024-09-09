1d0a0f3165f1edad34007ca7cc0cc40ad198e14e
Ivan00100 <ikonstandoglo6@gmail.com>
2024-09-09 18:57:21 +0300

    homework-12.1.2

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let link = "";
 
 document.getElementById("setLink").addEventListener("click", function () {
   link = prompt("Enter a link to go:");
+
+  if (link && !link.startsWith("http://") && !link.startsWith("https://")) {
+    link = "https://" + link;
+  }
 });
 
 document.getElementById("goLink").addEventListener("click", function () {
